61e79073e9c7235cba9bf777528727f713c2dfcf
bagasstr <bagassatrio2805@gmail.com>
2024-06-19 02:45:06 +0700

    change url endpoint

--- src/app/post/[id]/page.jsx
+++ src/app/post/[id]/page.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 
 const detailPost = async (id) => {
-  const res = await fetch(`/api/detailPost/${id}`, {
+  const res = await fetch(`https:localhost:3000/api/detailPost/${id}`, {
     cache: "no-cache",
   });
   const data = await res.json();
   return data;
 };
+
 const page = async ({ params }) => {
   const id = params.id;
   const post = await detailPost(id);
 
-  return <div>page</div>;
+  return (
+    <div>
+      {post.map((post) => (
+        <h1>{post.title}</h1>
+      ))}
+    </div>
+  );
 };
 
 export default page;
